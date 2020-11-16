51fe27fdea98621fd4299eb8cfe5ef388a89ad4b
David Urvoy <daviid.urvoy@gmail.com>
2020-11-16 23:45:23 +0000

    setup typescript and init src architecture

---
+++ src/routes/index.ts
@@ -0,0 +1,19 @@
+import express, {Request, Response} from 'express'
+
+let index_router = express.Router();
+
+let pgp = require('pg-promise')();
+let db = pgp('postgres://postgres:passwd@postgres:5432/postgres')
+
+
+/* GET home page. */
+export default index_router.get('/', (_: Request, response: Response) => {
+  response.render('index', { title: 'Express' });
+});
+
+index_router.get('/person', (_: Request, response: Response) => {
+	// let people = db.one('SELECT * from person')
+	// 	.then(people => console.log({people}))
+	// 	.catch(error => console.log('Connection error'))
+	response.render('index', { title: 'get Person' })
+});
